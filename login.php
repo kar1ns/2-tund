00a30a44d6bd47cf046b2f3331c2fa29f2d563b1
Karin Salu <kar1ns@greeny.cs.tlu.ee>
2015-09-09 13:22:05 +0300

    log in page2

--- login.php
+++ login.php
@@ -1,6 +1,32 @@
 <?php
 	//login.php
-
+	$email_error = "";
+	$password_error ="";
+	
+	//echo $_POST["email"];  //ANNAB MUUTUJA MIS AADRESSIREAL ON
+	// kontrollime et keeegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		//echo "keegi vajutas nuppu";
+		
+		//kontrollin et e-mail ei ole tühi
+		
+		if( empty($_POST["email"])){
+			$email_error = "See väli on kohustuslik";
+		}
+		//kontrollin, et parool ei ole tühi
+		
+		if( empty($_POST["password"])){
+			$password_error = "See väli on kohustuslik";
+		} else {
+			
+			if(strlen ($_POST["password"]) < 8){
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk";
+			}
+		}
+	}
+	
+	
+	
 ?>
 <html>
 <head>
@@ -8,10 +34,12 @@
 </head>
 <body>
 	<h2>Log in</h2>
-		<input type="email" placeholder="E-mail"> <br><br>
-		<input type="password" placeholder="Password"> <br><br>
-		<input type="submit" value="Log in">
-
+	
+		<form action="login.php" method="post">
+			<input name="email" type="email" placeholder="E-mail"> <?php echo $email_error; ?><br><br>
+			<input name="password"type="password" placeholder="Password"> <?php echo $password_error; ?> <br><br>
+			<input type="submit" value="Log in">
+		</form>
 	<h2>Creat user</h2>
 </body>
 
