19851d8c01f2b845be58f6c894e3e01a5f8f424f
Karin Salu <kar1ns@greeny.cs.tlu.ee>
2015-09-09 11:36:15 +0300

    first if

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,16 @@
 
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+	$age = 5;
+	
+	//testib loogikat, juhul kui vanus väiksem kui 18
+	//siis kirjutab "alaealine, muul juhul "täisealine"
+	if($age < 18){
+		echo "alaelaine";
+	} else {
+		echo "täisealine";
+	}
+	
+?>
