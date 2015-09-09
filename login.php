89cbfa91f283d480e185cca5c4aa2f1d0c9d0237
Karin Salu <kar1ns@greeny.cs.tlu.ee>
2015-09-09 12:38:55 +0300

    log in page

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+	//login.php
+
+?>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h2>Log in</h2>
+		<input type="email" placeholder="E-mail"> <br><br>
+		<input type="password" placeholder="Password"> <br><br>
+		<input type="submit" value="Log in">
+
+	<h2>Creat user</h2>
+</body>
+
+
+</html>
